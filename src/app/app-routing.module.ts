71e196f97cd1ea353c597588b987398167874ca0
Vivek Singh <VivekK.Singh@lntinfotech.com>
2023-05-10 19:54:13 +0530

    Initial template of WebApp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProjectsComponent } from './projects/projects.component';
+import { ProfileComponent } from './profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', component: ProfileComponent },
+  {
+    path: "projects",
+    pathMatch: 'full',
+    component: ProjectsComponent
+  },
+  // {
+  //   path: '**',
+  //   redirectTo: 'profile',
+  //   pathMatch: 'full'
+  // }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProfileComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
